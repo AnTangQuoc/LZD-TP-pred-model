8e4f8b01a076dbb71d17feb62440c70918bcc9d1
AnTangQuoc <antq2000@gmail.com>
2024-02-03 23:17:14 +0700

    increase MCMC iterations

---
+++ notebooks/BMA-model-no-initial-var.r
@@ -0,0 +1,14 @@
+library(BAS)
+run_bas_glm <- function(data, formula) {
+  bas.glm(
+    formula,
+    data = data,
+    family = binomial(),
+    MCMC.iterations = 200000,
+    # 100000 takes 1.5 mins, should be NULL (run until convergence, ~2.5 hrs)
+    method = "MCMC",
+    include.always = ~1,
+    laplace = TRUE,
+    renormalize = TRUE
+  )
+}
