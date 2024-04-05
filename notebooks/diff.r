1e0e69bf0080ba27c3ce68886534490201e886b2
AnTangQuoc <antq2000@gmail.com>
2024-04-06 00:05:47 +0700

    begin analyze survey

--- notebooks/diff.r
+++ notebooks/diff.r
@@ -1,10 +1,13 @@
 library(tidyverse)
 library(readxl)
 
-df1 <- read_xlsx("D:/R Working Directory/LZD-TP-pred-model-backup/All_data1.xlsx") |>
+df1 <- read_xlsx("D:/R Working Directory/LZD-TP-pred-model-backup/All_data2.xlsx") |>
   select(-"patient_name") |>
   select(starts_with("patient"), site, charlson, starts_with("dept"), starts_with("invasive"), starts_with("baseline"), starts_with("LZD"), starts_with("comorb"), starts_with("comed"), starts_with("infect"), flag_ADR_TP_ID) |>
   arrange(site) |>
+  mutate(
+    across(c("baseline_date", "LZD_start", "LZD_end"), ~ as_date(.x)),
+  ) |>
   group_by(site) |>
   arrange(patient_ID, .by_group = TRUE) |>
   ungroup()
@@ -19,4 +22,4 @@ df2 <- data_patient_complete |>
   ungroup() |>
   mutate(patient_ID = str_replace(patient_ID, "_1", ""))
 
-(compare_results <- waldo::compare(df1, df2, max_diffs = Inf))
+(compare_results <- waldo::compare(df1[36:44], df2[36:44], max_diffs = Inf))
