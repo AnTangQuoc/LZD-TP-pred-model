91b2e24982e54139450c3f4d07e339d06e82ad17
AnTangQuoc <antq2000@gmail.com>
2024-04-25 00:32:48 +0700

    add more strategies and black box model

--- notebooks/BMA-model-no-initial-var.r
+++ notebooks/BMA-model-no-initial-var.r
@@ -6,7 +6,6 @@ run_bas_glm <- function(data, formula, ...) {
     family = binomial(),
     # MCMC.iterations = 200000,
     # 100000 takes 1.5 mins, should be NULL (run until convergence, ~2.5 hrs)
-    method = "MCMC+BAS",
     laplace = TRUE,
     renormalize = TRUE,
     ...
